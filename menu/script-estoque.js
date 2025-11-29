b1eb60f5eb48d351a0908465d12202f2d2be1b28
IgorBarbosa87 <igor.talysson@souunit.com.br>
2025-11-29 11:37:04 -0300

    Delete menu directory

--- menu/script-estoque.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Lógica para o botão Exportar (simulação)
-    const exportBtn = document.querySelector('.export-btn');
-    if (exportBtn) {
-        exportBtn.addEventListener('click', function() {
-            alert('Funcionalidade de Exportar em desenvolvimento.');
-        });
-    }
-});
